9c4ba2c2178cb6f8583b2adf5cb9ee0145b2e7ce
sunaoka <sunaoka@pocari.org>
2024-04-23 14:24:09 +0900

    Change TsRange type to Date facade

--- src/Types/TsRange.php
+++ src/Types/TsRange.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace Sunaoka\LaravelPostgres\Types;
 
-use Carbon\CarbonImmutable;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Date;
 
 /**
- * @extends Range<CarbonImmutable, string>
+ * @extends Range<Carbon, string>
  */
 class TsRange extends Range
 {
     /**
      * @param  string  $boundary
      */
-    protected function transform($boundary): CarbonImmutable
+    protected function transform($boundary): Carbon
     {
-        return CarbonImmutable::parse($boundary);
+        return Date::parse($boundary);
     }
 }
